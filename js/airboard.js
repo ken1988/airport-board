0a4151f481f3c172a8f4b4d11e1987b897d0bd22
novilen.gov@gmail.com <novilen.gov@gmail.com>
2015-09-27 04:00:18 +0900

    #1 update timepicker js. enabled arrival time field.

--- js/airboard.js
+++ js/airboard.js
@@ -2,15 +2,36 @@
  *
  */
  $(document).ready(function() {
-    $('#dept_time').timepicker();
+    $('#dept_time').timepicker({
+    	template: 'modal',
+    	minuteStep: 30,
+    	showMeridian: false,
+    	showInputs: false
+    });
 
-    $('#dept_time').change(function(){
-    	var dept_time = $(this).val();
-    	var dist = $('#dist').val();
-    	var arrv_time = dept_time + dist;
+    function Chg_artime(){
+    	var dept_time = $('#dept_time').val();
+    	var dist = Number($('#dist').val());
+    	var depts = dept_time.split(':');
+    	var dept_h = Number(depts[0]);
+    	var dept_h2 = dept_h + dist;
+
+    	if (dept_h2 > 23){
+    		dept_h2 = dept_h2 - 24;
+    	}
+
+    	var arrv_time = dept_h2+':'+depts[1];
 
     	$('#arrv_time').val(arrv_time);
+    }
+
+    $('#dept_time').change(function(){
+    	Chg_artime();
     });
+    $('#dist').change(function(){
+    	Chg_artime();
+    });
+
     $('#airline').change(function(){
     	$('#comp_abb').text($(this).children('option:selected').attr('data-abb'));
     });
